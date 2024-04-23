8adc21311f85b5bf1328540806d4bafe69547b76
JoaquinVarelaCenco <joaquin.varela@cencosud.com>
2024-04-22 22:56:50 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.miultimaprueba"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.miultimaprueba"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    ndkVersion = "25.1.8937393"
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.camera:camera-core:1.2.3")
+    implementation ("androidx.camera:camera-camera2:1.2.3")
+    implementation ("androidx.camera:camera-lifecycle:1.2.3")
+    implementation ("androidx.camera:camera-video:1.2.3")
+    implementation ("androidx.camera:camera-viewfinder:1.3.0-beta02")
+    implementation ("androidx.camera:camera-view:1.2.3")
+    implementation ("androidx.camera:camera-extensions:1.2.3")
+    implementation ("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation ("org.tensorflow:tensorflow-lite-select-tf-ops:2.14.0")
+
+    implementation ("com.google.mediapipe:hands:0.10.9")
+
+}
